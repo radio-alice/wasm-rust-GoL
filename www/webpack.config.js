1edbd8ba2621337d5c8cebae76bdaa7872979de4
Zach Sherman <zachsherman@cs.uchicago.edu>
2019-04-13 15:46:57 -0500

    update webpack config for deployment

--- www/webpack.config.js
+++ www/webpack.config.js
@@ -1,14 +1,25 @@
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const path = require('path');
+const express = require('express');
 
 module.exports = {
   entry: "./bootstrap.js",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "bootstrap.js",
+    filename: "bootstrap.js"
   },
   mode: "development",
   plugins: [
     new CopyWebpackPlugin(['index.html'])
   ],
+  devServer: {
+    disableHostCheck: true,
+    host: '0.0.0.0',
+    contentBase: path.join(__dirname, ''),
+    compress: true,
+    setup (app) {
+      app.use('/gol',
+        express.static(path.join(__dirname, '')));
+    }
+  }
 };
